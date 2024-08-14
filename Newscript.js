e3940166eb3ac4820565f454989dfd6e0bd35c92
luckylluuks <153525439+luckylluuks@users.noreply.github.com>
2024-08-14 13:24:13 +0200

    Update Newscript.js

--- Newscript.js
+++ Newscript.js
@@ -1,1 +1,22 @@
+import * as THREE from 'three';
+
+// scene
+const scene = THREE.Scene()
+
+//Object
+const Geometry = new THREE.BoxGeometry(1, 1, 1)
+const Material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const Mesh = new THREE.Mesh(Geometry, Material)
+scene.add(Mesh)
+
+//Camera
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const Camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)scene.add(Camera)
+
+
+
 
